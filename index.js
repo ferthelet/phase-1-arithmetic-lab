affd32fa1f883640d760b7b90c9d09eb9dce82f0
Juan Salazar <ferthelet@gmail.com>
2023-03-23 15:32:24 -0500

    adds remainder

--- index.js
+++ index.js
@@ -8,9 +8,9 @@ let multiply = num1 * num2;
 let random = Math.floor(Math.random() * 1000) + 1;
 
 // is an equation that calculates a remainder that is equal to 4
-function mod(num1, num2) {
-    return num1 * num2;
-}
+let num3 = 14;
+let num4 = 10;
+let mod = num3 % num4;
 
 // will return 20 as the highest number in the set
 function max(num1, num2) {
