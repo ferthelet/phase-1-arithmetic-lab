af2d50b3dd9ffd5764c90ad0a72e8f75511fb707
Juan Salazar <ferthelet@gmail.com>
2023-03-23 15:18:28 -0500

    adds multiplication

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
-
 // Write your code here
+// is an equation whose multiplied results will equal 62
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+
+// generates a random integer greater than 0
+function random(num1, num2) {
+    return num1 * num2;
+}
+
+// is an equation that calculates a remainder that is equal to 4
+function mod(num1, num2) {
+    return num1 * num2;
+}
+
+// will return 20 as the highest number in the set
+function max(num1, num2) {
+    return num1 * num2;
+}
